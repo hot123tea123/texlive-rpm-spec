2ee9c98d23898c8c83273508b5349c0e998aae4c
Darcy Hu <hot123tea123@gmail.com>
2014-10-21 23:26:32 +0800

    Add english comments

--- init_texlive.tlpdb.sh
+++ init_texlive.tlpdb.sh
@@ -1,43 +1,92 @@
 #!/bin/sh
 #
-# 将 TLCore, Package, Collection, Scheme 分开
+# clean files generated last time run this script
+rm -rvf *.stanza *.list *.depend
+#
+#  split TLCore, Package, Collection, Scheme stanza from texlive.tlpdb.orig
 sed -e '/./{H;$!d;}' -e 'x;/category TLCore/!d;' texlive.tlpdb.orig > TLCore.stanza
 sed -e '/./{H;$!d;}' -e 'x;/category Package/!d;' texlive.tlpdb.orig > Package.stanza
 sed -e '/./{H;$!d;}' -e 'x;/category Collection/!d;' texlive.tlpdb.orig > Collection.stanza
 sed -e '/./{H;$!d;}' -e 'x;/category Scheme/!d;' texlive.tlpdb.orig > Scheme.stanza
-# 获取名称列表
-# TLCore
+#
+#
+# get items' name list
+#
+# TLCore list
+# remove: 00texlive.config, 00texlive.image, 00texlive.installation, 00texlive.installer
+# these items are installation configuration, should not be packaged in a RPM.
+sed -i -e '/./{H;$!d;}' -e 'x;/name 00texlive.config\|name 00texlive.image\|name 00texlive.installation\|name 00texlive.installer/d;' TLCore.stanza
+# remove architecture dependency TLCore items
+sed -i -e '/./{H;$!d;}' -e 'x;/i386-linux\|x86_64-linux\|alpha-linux\|amd64-freebsd\|amd64-kfreebsd\|amd64-netbsd\|armel-linux\|armhf-linux\|i386-cygwin\|i386-freebsd\|i386-kfreebsd\|i386-netbsd\|i386-solaris\|mipsel-linux\|powerpc-linux\|sparc-solaris\|universal-darwin\|win32\|x86_64-cygwin\|x86_64-darwin\|x86_64-solaris/d;' TLCore.stanza
 sed '/^name /!d' TLCore.stanza > TLCore.list
 sed -i 's/name //' TLCore.list
-sed -i '/^$/d' TLCore.list
-# 00texlive.config, 00texlive.image, 00texlive.installation, 00texlive.installer 这几个是安装配置什么的，不需要
-sed -i '/^00texlive.config$\|^00texlive.image$\|^00texlive.installation$\|^00texlive.installer$/d' TLCore.list
-# 删除所有的 name.ARCH，只需要获得名称列表
-sed -i '/\./d' TLCore.list
-sed -i '/^$/d' TLCore.list
-# Package
+# end TLCore list
+#
+# Package list
+# remove architecture dependency TLCore items, too
+sed -i -e '/./{H;$!d;}' -e 'x;/i386-linux\|x86_64-linux\|alpha-linux\|amd64-freebsd\|amd64-kfreebsd\|amd64-netbsd\|armel-linux\|armhf-linux\|i386-cygwin\|i386-freebsd\|i386-kfreebsd\|i386-netbsd\|i386-solaris\|mipsel-linux\|powerpc-linux\|sparc-solaris\|universal-darwin\|win32\|x86_64-cygwin\|x86_64-darwin\|x86_64-solaris/d;' Package.stanza
 sed '/^name /!d' Package.stanza > Package.list
 sed -i 's/name //' Package.list
-sed -i '/^$/d' Package.list
-# Collection
+# end Package list
+#
+# Collection list
 sed '/^name /!d' Collection.stanza > Collection.list
 sed -i 's/name //' Collection.list
-sed -i '/^$/d' Collection.list
-# Scheme
+# end Collection list
+#
+# Scheme list
 sed '/^name /!d'  Scheme.stanza > Scheme.list
 sed -i 's/name //' Scheme.list
-sed -i '/^$/d' Scheme.list
+#end Scheme list
 #
-# 处理 .stanza 文件
-# TLCore
-# 删除 00texlive.config, 00texlive.image, 00texlive.installation, 00texlive.installer 的 stanza
-sed -i -e '/./{H;$!d;}' -e 'x;/00texlive.config\|00texlive.image\|00texlive.installation\|00texlive.installer/d;' TLCore.stanza
-# 提取 i386-linux 的 stanza
-sed -e '/./{H;$!d;}' -e 'x;/i386-linux/!d;' TLCore.stanza > TLCore.i386-linux.stanza
-# 删除 x86_64-linux 的 stanza
-sed -e '/./{H;$!d;}' -e 'x;/x86_64-linux/!d;' TLCore.stanza > TLCore.x86_64-linux.stanza
-# 删除所有包含架构相关的 stanza，只保留架构无关了。大部分都是文档包
-sed -e '/./{H;$!d;}' -e 'x;/i386-linux\|x86_64-linux\|alpha-linux\|amd64-freebsd\|amd64-kfreebsd\|amd64-netbsd\|armel-linux\|armhf-linux\|i386-cygwin\|i386-freebsd\|i386-kfreebsd\|i386-netbsd\|i386-solaris\|mipsel-linux\|powerpc-linux\|sparc-solaris\|universal-darwin\|win32\|x86_64-cygwin\|x86_64-darwin\|x86_64-solaris/d;' TLCore.stanza > TLCore.noarch.stanza
+#
+# split collections' stanza from Collection.stanza
+for clt in `cat Collection.list`; do
+	sed -e '/./{H;$!d;}' -e "x;/name $clt/!d;" Collection.stanza > $clt.stanza
+done
+# generate Collections' dependency
+# all items list in $clt.depend is dependency item name
+for clt in `cat Collection.list`; do
+	sed -n '/^depend/p' $clt.stanza > $clt.depend
+done
+for clt in `cat Collection.list`; do
+	sed -i 's/depend //' $clt.depend
+done
+# dependency between collections
+for clt in `cat Collection.list`; do
+	sed '/^collection-/!d' $clt.depend > $clt.collection.depend
+done
+# delete empty .collection.depend file, which means no dependency
+for clt in `cat Collection.list`; do
+	if test -s $clt.collection.depend; then
+	echo "Do nothing" > /dev/null
+	else
+		rm -vf $clt.collection.depend
+	fi
+done
+#
+#
+# split packages' stanza from Package.stanza
+for pkg in `cat Package.list`; do
+	sed -e '/./{H;$!d;}' -e "x;/name $pkg/!d;" Package.stanza > $pkg.stanza
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
