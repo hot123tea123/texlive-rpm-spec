629ea106ca5478bc5f0cafd6316b494b26f5e326
Darcy Hu <hot123tea123@gmail.com>
2014-11-30 20:28:49 +0800

    REALLY FUCKING FIXED

--- generate-font-maps.sh
+++ generate-font-maps.sh
@@ -5,9 +5,18 @@ rm -vf *.maps
 # generate font maps file
 for clt in `cat Collection.list`; do
   for pkg in `cat $clt.package.depend`; do
-    sed '/^execute addMap\|execute addMixedMaps/!d' $pkg.stanza>> $clt.maps
+    sed '/^execute addMap\|execute addMixedMaps/!d' $pkg.stanza >> $clt.maps
   done
 done
+# delete empty files
+for tmp in *.maps; do
+  if test -s $tmp; then
+  echo "Do nothing!" > /dev/null
+  else
+  rm -vf $tmp
+  fi
+done
+  
 #
 for tmp in *.maps; do
   sed -i 's/execute add//' $tmp
