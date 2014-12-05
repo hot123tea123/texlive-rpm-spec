97d4ed4069f596393d68c8a7b1f64316fbc96f01
Darcy Hu <hot123tea123@gmail.com>
2014-12-05 21:56:15 +0800

    sometimes you just herp the derp so hard it herpderps

--- generate-package-list.sh
+++ generate-package-list.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Package list
-# remove architecture dependency TLCore items, too
 echo "generate Package list..."
 sed '/^name /!d' Package.stanza > Package.list
 sed -i 's/name //' Package.list
